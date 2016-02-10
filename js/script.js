c00a1948f6e8034ededfb4d9c10771de28f1edff
jnguyen7410 <jnguyen7410@gmail.com>
2016-02-10 00:07:30 -0800

    Fixed side nav-bar. Still working on getting modal to show properly

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+$(document).ready(function(){
+	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	$('.modal-trigger').leanModal({
+		dismissible: true, // Modal can be dismissed by clicking outside of the modal
+		opacity: .5, // Opacity of modal background
+		in_duration: 300, // Transition in duration
+		out_duration: 200, // Transition out duration
+		ready: function() { alert('Ready'); }, // Callback for Modal open
+		complete: function() { alert('Closed'); } // Callback for Modal close
+	});
+	$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+});
