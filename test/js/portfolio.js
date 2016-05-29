69a56bf2c1fca6bbf39d6efad883ad608caff7a1
jnguyen7410 <jnguyen7410@gmail.com>
2016-05-29 01:34:17 -0700

    Added changes to prep for dynamic graph width/height

--- test/js/portfolio.js
+++ test/js/portfolio.js
@@ -1,25 +1,26 @@
 // JavaScript Document
 $(document).ready(function(){
 	var ctx = document.getElementById("skillBars");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Java", "Apex", "Salesforce", "Git/hub", "Bash/Batch", "JS", "CSS", "Android Java", "Bootstrap", "Python"],
-        datasets: [{
-            label: 'Number of Years in Experience',
-            data: [6, 1, 1, 1, 3, 6, 4, 1, 1, 4],
-						backgroundColor: "rgba(253,61,80,1)"
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-
+	//ctx.canvas.width = ;
+	//ctx.canvas.height = ;
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ["Java", "Apex", "Salesforce", "Git/hub", "Bash/Batch", "JS", "CSS", "Android Java", "Bootstrap", "Python"],
+	        datasets: [{
+	            label: 'Number of Years in Experience',
+	            data: [6, 1, 1, 1, 3, 6, 4, 1, 1, 4],
+							backgroundColor: "rgba(253,61,80,1)"
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
 });
