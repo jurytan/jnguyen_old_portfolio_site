a672e7c002bcb007ba2f8d38b4c5f27d85a31bdf
jnguyen7410 <jnguyen7410@gmail.com>
2016-06-10 23:56:23 -0700

    Added a new placeholder mission for 006 for now

---
+++ test/js/missions/mission-006.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+$(document).ready(function(){
+	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	$('.modal-trigger').leanModal({
+		dismissible: true, // Modal can be dismissed by clicking outside of the modal
+		opacity: .5, // Opacity of modal background
+		in_duration: 300, // Transition in duration
+		out_duration: 200 // Transition out duration
+	});
+	$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+	$('.scrollspy').scrollSpy();
+	$('.parallax').parallax();
+	$('.materialboxed').materialbox();
+	$("#submit").click(function(e){
+		checkPassword();
+	});
+	$('form input').keydown(function(e) {
+		if (e.keyCode == 13) {
+			event.preventDefault();
+			checkPassword();
+			$("#modal").openModal();
+			$("form input").blur();
+		}
+	});
+
+});
+$.cookie("canAccessNextLevel", "false");
+
+function checkPassword() {
+	if ($.cookie("canAccessNextLevel") == true) {
+		$("#resultHeader").text('Congrats!');
+		$("#resultText").text('Nice! Get it? \'Cookie\' jar? It\'s okay, I crack myself up...');
+		$("#modal").css('background-color', '#2ecc71');
+		$(".modal-footer").addClass("importantGreen");
+		$(".modal-footer a").attr("href", "mission-005.html")
+	} else if ($("#password").val() == ""){
+		$("#resultHeader").text('Error!');
+		$("#resultText").text('You didn\'t enter anything. Try again!');
+		$("#modal").css('background-color', '#e74c3c');
+		$(".modal-footer").addClass("importantRed");
+	} else {
+		$("#resultHeader").text('Sorry!');
+		$("#resultText").text('You entered the wrong password, buddy. Try again!');
+		$("#modal").css('background-color', '#e74c3c');
+		$(".modal-footer").addClass("importantRed");
+	}
+}
