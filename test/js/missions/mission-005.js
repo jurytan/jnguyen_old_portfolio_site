73eacd1e6d097bfefb73d5031c61523f98c5b088
jnguyen7410 <jnguyen7410@gmail.com>
2016-06-11 23:09:10 -0700

    Changed the theme of the dropdown menu to match rest of theme

--- test/js/missions/mission-005.js
+++ test/js/missions/mission-005.js
@@ -36,7 +36,7 @@ function checkPassword() {
 		$("#resultText").text('Nice! Get it? \'Cookie\' jar? It\'s okay, I crack myself up...');
 		$("#modal").css('background-color', '#2ecc71');
 		$(".modal-footer").addClass("importantGreen");
-		$(".modal-footer a").attr("href", "mission-005.html")
+		$(".modal-footer a").attr("href", "mission-006.html")
 	} else if ($("#password").val() == ""){
 		$("#resultHeader").text('Error!');
 		$("#resultText").text('You didn\'t enter anything. Try again!');
