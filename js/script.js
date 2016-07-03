51dedf88feaf8f1df267b3e7e6ad6d69eb9d1359
Jonathan Nguyen <jnguyen@modeln.com>
2016-07-03 16:58:21 -0400

    Restructured site to move new revision into production. Will continue cleaning up the site

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		dismissible: true, // Modal can be dismissed by clicking outside of the modal
 		opacity: .5, // Opacity of modal background
 		in_duration: 300, // Transition in duration
-		out_duration: 200, // Transition out duration
+		out_duration: 200 // Transition out duration
 	});
 	$('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
@@ -15,6 +15,7 @@ $(document).ready(function(){
 	$('.scrollspy').scrollSpy();
 	$('.parallax').parallax();
 	$('.materialboxed').materialbox();
+	$(".dropdown-button").dropdown();
 	$.ajax({
 			url: 'https://api.github.com/repos/jnguyen7410/NguyeningCode',
 			type: 'GET',
